33c69e15f5c742a22458beb8c4c0de4c84957749
David Allison <62114487+david-allison-1@users.noreply.github.com>
2021-05-13 23:40:21 +0100

    Add Unit Test Project
    We're getting close to done, would be nice to add some regression tests

---
+++ Cregeen-Dictionary.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Cregeen_Dictionary.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
