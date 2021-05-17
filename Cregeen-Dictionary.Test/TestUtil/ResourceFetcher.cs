13c8ed4a35a45a6bec9ed0402ae1bc10150b8135
David Allison <62114487+david-allison-1@users.noreply.github.com>
2021-05-18 00:54:59 +0100

    chore: extract ResourceFetcher

---
+++ Cregeen-Dictionary.Test/TestUtil/ResourceFetcher.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace Cregeen_Dictionary.Test.TestUtil
+{
+    public static class ResourceFetcher
+    {
+        public static string GetResource(string file)
+        {
+            using var stream = typeof(Tests).Assembly.GetManifestResourceStream(file);
+
+            if (stream == null)
+            {
+                var names = typeof(Tests).Assembly.GetManifestResourceNames();
+            }
+
+            using TextReader tr = new StreamReader(stream);
+            return tr.ReadToEnd();
+
+        }
+    }
+}
