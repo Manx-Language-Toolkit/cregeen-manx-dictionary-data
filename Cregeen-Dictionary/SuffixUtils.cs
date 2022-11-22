4d793b959e1e0a37b2714c7c496f862c27a2c8c2
David Allison <62114487+david-allison@users.noreply.github.com>
2022-11-22 01:20:45 +0000

    feat: strip additional suffixes
    Improves a number of definitions

--- Cregeen-Dictionary/SuffixUtils.cs
+++ Cregeen-Dictionary/SuffixUtils.cs
@@ -10,11 +10,17 @@ public static class SuffixUtils
     public static readonly List<string> Suffixes = new()
     {
         "‑agh, 77", 
+        "‑ail, 78",
+        "‑al, 79",
         "‑ee, 80", 
+        "‑eil, 81",
+        "‑ey, 82",
         "‑in, 83", 
         "‑ins, 84", 
+        "‑it, 85",
         "‑ym, 86", 
         "‑yms, 87", 
-        "‑ys, 88"
+        "‑ys, 88",
+        " 89"
     };
 }
